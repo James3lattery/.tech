efba054853710c18f2d80b5b14766b41f3ebdb2d
James Slattery <james@slattery.tech>
2017-03-24 14:04:30 -0700

    js skyward up

--- static/js/fileselect.js
+++ static/js/fileselect.js
@@ -4,9 +4,14 @@ $(document).ready(function () {
        $('.del').removeClass('disabled');
    });
    $('input').addClass('form-control');
+
+   // skyward
    	$('.querC').click(function(){
 		var start = $('.start').val();
 		var limit = $('.limit').val();
+    if ( limit == " ") {
+      limit = start + 50;
+    }
 		for (i = start; i < limit; i++) {
 			$('.spawn').append('<div onclick="alert(' + i + ')" ><img style="float: left; cursor: pointer;" width="100" height="150" class="thumbnail" src="https://www2.wrdc.wa-k12.net/pictures/bainbrs/0' + i + '.JPG" /></div>');
 		}
